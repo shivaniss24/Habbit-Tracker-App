f57657fa8f23a7e85dcee435da50bc3ee56a1b07
shivam soni <shivamsoni00300@gmail.com>
2023-06-18 22:07:42 +0530

    updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Card = ({ habit }) => {
-    const { id, name, image, repeat, goal, time_of_day, start_date, isArchived } = habit;
+    const { id, name, image, repeat, goal, time_of_day, start_date } = habit;
     return (
         <div>
             <div className="col s12 m3">
                 <div className="card medium">
                     <div className="card-image">
-                        <img src={image} />
+                        <img alt={id} src={image} />
                         <span className="card-title">{name}</span>
                     </div>
                     <div className="card-content">
@@ -19,9 +19,9 @@ const Card = ({ habit }) => {
                         </ul>
                     </div>
                     <div className="card-action">
-                        <a>Edit</a>
-                        <a>Archive</a>
-                        <a>Delete</a>
+                        <button>Edit</button>
+                        <button>Archive</button>
+                        <button>Delete</button>
                     </div>
                 </div >
             </div >
